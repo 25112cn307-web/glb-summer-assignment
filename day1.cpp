9e233ffad07f5df0f2551eacc67b3e94044e456e
25112cn307-web <25112cn307@glbitm.ac.in>
2026-06-30 20:46:07 -0700

    Add files via upload

---
+++ day1.cpp
@@ -0,0 +1,61 @@
+/*#include <iostream>
+  using namespace std;
+  int main (){
+  
+    int N;
+   cout<<"enter a number:";
+   cin>>N;
+   int sum =N*(N+1)/2;
+   cout<<"sum of first"<<N<<"natural numbers="<<sum;
+   return 0;
+  }*/
+
+
+/*
+  #include<iostream>
+     using namespace std ;
+     int main(){
+      int n;
+      long factorial =1;
+      cout<<"enter a number:";
+      cin>>n;
+      for( int i=1;i<=n;i++)  
+    {
+        factorial*=i;
+    }
+    cout<<"factorial of "<< n<<"="<<factorial;
+    return 0;
+}
+     */
+/*
+       #include<iostream>
+     using namespace std ;
+     int main(){
+      int num, count=0;
+      cout<<"enter a number:";
+      cin>>num;
+      while(num!=0){
+        num=num/10;
+        count++;
+      }
+      cout<<"number of digits ="<<count;
+      return 0;
+    }
+      */
+
+
+/*
+        #include<iostream>
+     using namespace std ;
+     int main(){
+      int num;
+       cout<<"enter a number:";
+       cin>>num;
+       cout<< "multiplication table of "<< num<< endl;
+       for (int i=1; i<=10;i++)
+       {
+        cout<<num<<"x"<<i<<"="<<num*i<<endl;
+       }
+       return 0;
+    }
+       */
